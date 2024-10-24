05f669171b8ea080857ea899c63dfbaef652d6dd
Felipe Dos Santos <felipe.lpdsantos@gmail.com>
2024-10-23 21:18:50 -0600

    done w6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.IO.Compression;
+
 public class Node
 {
     public int Data { get; set; }
@@ -11,7 +13,6 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
 
         if (value < Data)
         {
@@ -21,25 +22,47 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
                 Right.Insert(value);
+        } else {
+
         }
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+
+        if (value == Data) {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+
+        int lHeight = Left == null ? 0:Left.GetHeight();
+        int rHeight = Right == null ? 0:Right.GetHeight();
+
+        return 1 + Math.Max(lHeight, rHeight);
     }
 }
